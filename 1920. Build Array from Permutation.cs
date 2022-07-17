6650f3a25104d1e3f0d7fc03d84e7c13171a95fa
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2022-07-17 18:02:59 +0900

    1920

---
+++ 1920. Build Array from Permutation.cs
@@ -0,0 +1,19 @@
+﻿/* Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive). */
+
+public class Solution {
+    int x;
+    
+    public int[] BuildArray(int[] nums) {
+        int[] ans = new int[nums.Length];        
+        //nums = [5,0,1,2,3,4];
+        
+        for(int i=0; i < nums.Length; i++){
+            ans[i] = nums[nums[i]];
+            }
+        
+        return ans;
+    }
+}
+
+
